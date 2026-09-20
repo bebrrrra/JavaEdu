39e3247fbeb67065c9ce8f1bad945acd901c4306
Vera <mvk635@gmail.com>
2026-09-21 00:19:53 +0300

    lab2 task2 completed

--- Lets try first prac/src/lab2/task2/TestBall.java
+++ Lets try first prac/src/lab2/task2/TestBall.java
@@ -5,12 +5,17 @@ public class TestBall {
         Ball ball1 = new Ball(0.0, 0.0);
         Ball ball2 = new Ball();
 
+        ball1.setX(3.0);
+        ball1.setY(4.0);
         System.out.println(ball1.getX());
         System.out.println(ball1.getY());
 
+        ball2.setXY(6.0, 4.0);
+        ball2.move(2.0, 2.0);
         System.out.println(ball2.getX());
         System.out.println(ball2.getY());
 
-
+        System.out.println(ball1);
+        System.out.println(ball2);
     }
 }
